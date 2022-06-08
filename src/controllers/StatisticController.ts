a507198f0833033d4f2c6d7af4ce8cd5e04de694
pessolatohenrique <pessolatohenrique@gmail.com>
2022-06-08 20:31:56 -0300

    utilizando cache nas estatisticas

--- src/controllers/StatisticController.ts
+++ src/controllers/StatisticController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { DetailRequest } from "../interface/DetailRequest";
 import { Statistic } from "../models/Statistic";
+import RedisWrapper from "../config/RedisWrapper";
+import { DatabaseSummarized } from "../interface/Database";
 
 export class StatisticController {
   static async index(
@@ -9,7 +11,23 @@ export class StatisticController {
   ): Promise<Response> {
     try {
       const statisticModel = new Statistic();
+
+      const { fromCache } = req.query;
+      const redisWrapper = new RedisWrapper<DatabaseSummarized>();
+
+      const cacheKey = `notion:statistic:${req.params.id}`;
+      const cacheValue = await redisWrapper.isResultInCache(
+        Boolean(fromCache),
+        cacheKey
+      );
+
+      if (cacheValue) {
+        const cacheValueParsed = JSON.parse(cacheValue);
+        return res.status(200).json(cacheValueParsed);
+      }
+
       await statisticModel.generate(req.params.id);
+      redisWrapper.setWithParse(cacheKey, statisticModel.response);
       return res.status(200).json(statisticModel.response);
     } catch (e) {
       return res.status(500).json(e);
