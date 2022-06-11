3453afdbada21f6d2c75dd281e1597a377454159
pessolatohenrique <pessolatohenrique@gmail.com>
2022-06-10 21:56:06 -0300

    ativacao do cors

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const routes = require("./src/routes");
 import RequestHelper from "./src/utils/RequestHelper";
 import RedisWrapper from "./src/config/RedisWrapper";
 require("dotenv").config();
+
+const app = express();
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server Started");
